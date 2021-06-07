9cafc1c37b623708b32d99586e081b954224d4fb
yamilacapp <yamila_capp@hotmail.com>
2021-06-07 18:48:15 -0300

    subiendo cambios

--- contacto.php
+++ contacto.php
@@ -78,21 +78,7 @@ if ($_POST) { /* es postback */
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/fontawesome/fontawesome-free-5.13.1-web/css/all.min.css">
-    <link rel="stylesheet" href="css/fontawesome/fontawesome-free-5.13.1-web/css/fontawesome.min.css">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap">
-<link
-    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,400;1,500;1,700;1,900&display=swap"
-    rel="stylesheet">
-        <link rel="stylesheet" href="css/estilos.css">
-</head>
+<?php include_once("head.php"); ?>
 
 <body>
 
